e5273a2a90df67a1a5e22a44a98b5c62b8dd6e84
Mariano Manuel <mkma235@hotmail.com>
2021-05-25 21:41:31 -0400

    Project Setup

---
+++ StarWars/StarWars/DetailView.swift
@@ -0,0 +1,29 @@
+//
+//  DetailView.swift
+//  StarWars
+//
+//  Created by Mariano Manuel on 5/25/21.
+//
+
+import UIKit
+
+class DetailView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ StarWars/StarWars/MasterView.swift
@@ -0,0 +1,20 @@
+//
+//  MasterView.swift
+//  StarWars
+//
+//  Created by Mariano Manuel on 5/25/21.
+//
+
+import UIKit
+
+class MasterView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController!.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Star Wars"
+
+        // Do any additional setup after loading the view.
+    }
+
+}
